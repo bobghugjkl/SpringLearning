7199ef640c00ddcef1828af024b5929dc96e4bb8
bobghugjkl <2553348955@qq.com>
2023-12-12 21:06:00 +0800

    SpringIOC PartI

---
+++ SpringIOC/SpringIOCLearningI/src/main/java/com/xxxx/App.java
@@ -0,0 +1,19 @@
+package com.xxxx;
+
+import com.xxxx.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+        UserService userService = (UserService) ac.getBean("userService");
+        userService.test();
+    }
+}
